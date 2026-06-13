48b621980e7e76ab1253e75bdd53008b1e232a48
kay2seamus-design <kay2seamus@gmail.com>
2026-06-13 12:53:41 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -20,7 +20,7 @@ export const botConfig = {
     // 1 = Streaming
     // 2 = Listening
     // 3 = Watching
-    // 4 = Custom
+    // 4 = Made by Seamus
     // 5 = Competing
     activities: [
       {
